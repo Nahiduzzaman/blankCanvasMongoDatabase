95c3c1b91a5fcb93d112d44e76cbe73bee6ab153
nathan-j-brenner <nathan.j.brenner@gmail.com>
2016-06-02 10:16:57 -0700

    specified id field based on Shannon's example

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ MongoClient.connect('mongodb://localhost:27017/test', function (err,db){
     for (var i = 0; i < 100; i++) {
         for (var j = 0; j < 100; j++) {
             for (var k = 0; k < 100; k++) {
-                docs.push({ a: i, b: j, c: k });
+                var id = 100 * 100 * i + 100 * j + k;
+                docs.push({ _id: id, a: i, b: j, c: k });
             }
         }
         if(i%10 == 0){
@@ -22,5 +23,5 @@ MongoClient.connect('mongodb://localhost:27017/test', function (err,db){
         }
     }
     db.collection('example').insertMany(docs);
-    console.log(`Created ${x.length} docs in db test collection example`);
+    console.log(`Created ${docs.length} docs in db test collection example`);
 });
